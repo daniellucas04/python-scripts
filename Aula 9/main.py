d51d22a65967389bc1c7ff75a3bd16ca9e07a201
unknown <daniellsilva@outlook.com>
2023-03-09 20:28:02 -0300

    oop programming

--- Aula 9/main.py
+++ Aula 9/main.py
@@ -1,2 +1,26 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from model import *
+
+person_1 = Pessoa("José", "Souza")
+person_1.info()
+
+client = Cliente("Ana", "Ferreira", 1200)
+client.info()
+
+seller = Vendedor("Mauricio", "Sales", 12)
+seller.info()
+
+
+class Computer:
+    def __init__(self, memory, processor):
+        self.memory = memory
+        self.processor = processor
+
+    def my_pieces(self):
+        print(self.memory, self.processor)
+
+
+computer_1 = Computer(1, "AMD")
+computer_2 = Computer(4, "Intel")
+
+computer_1.my_pieces()
+computer_2.my_pieces()
